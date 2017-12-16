3a42bba077db0cd071cf3240877c144118c0ecdb
jfdelarosa <jfdelarosa@outlook.com>
2017-12-15 22:04:35 -0600

    Basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png" />
-    <HelloWorld />
-  </div>
+<template lang="pug">
+  #app
+    .grid
+      .layout(v-for="components in layout")
+        component(v-for="component in components",
+          v-bind:is="component.name",
+          :key="component.id") {{component.id}}
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Sortable from 'sortablejs';
+import card from './components/card';
+
+var layout = [
+  [
+    {
+      "name": "card",
+      "id": 1
+    }
+  ],
+  [
+    {
+      "name": "card",
+      "id": 2
+    }
+  ],
+  [
+    {
+      "name": "card",
+      "id": 3
+    },
+    {
+      "name": "card",
+      "id": 4
+    }
+  ]
+];
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    card
+  },
+  mounted(){
+    for(var i = 0; i <= 2; i++){
+      Sortable.create(document.getElementsByClassName('layout')[i], {
+        group: "ui",
+        animation: 150
+      });
+    }
+  },
+  data(){
+    return {
+      layout: layout
+    }
+	}
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  body{
+    background: #eee;
+  }
+  .grid{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-column-gap: 1rem;
+  }
 </style>
---
+++ src/components/card.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .card
+    .card__header Titulo {{order}}
+    .card__body
+      slot
+</template>
+
+<script>
+export default {
+  name: 'card',
+  props: ['order', 'width', 'height'],
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .card{
+    margin-bottom: 1rem;
+    &__header{
+      background: #eee;
+      border: 1px solid black;
+      border-bottom: none;
+      padding: 0.5rem;
+    }
+    &__body{
+      background: white;
+      border: 1px solid black;
+      padding: 0.5rem;
+    }
+  }
+</style>
